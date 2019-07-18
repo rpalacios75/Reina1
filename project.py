a0285cc7aed634a790a1292ced4f5e4c5d66e725
EC2 Default User <ec2-user@ip-172-31-30-94.us-east-2.compute.internal>
2019-07-18 19:17:34 +0000

    add/sub/mult/div calculator

--- project.py
+++ project.py
@@ -1,7 +1,7 @@
-operation = input ("Choose your operation")
+operation = input ("Choose your operation ")
      
-x = input ("What is your first number?")
-y = input ("What is your second number?")
+x = input ("What is your first number? ")
+y = input ("What is your second number? ")
 
 def add(x,y):
     answer = (int(x)+int(y))
@@ -11,12 +11,27 @@ def sub(x,y):
     answer = (int(x)-int(y))
     return (answer)
     
+def mul(x,y):
+    answer = (int(x)*int(y))
+    return (answer)
+
+def div(x,y):
+    answer = (int(x)/int(y))
+    return (answer)
+    
     
 if operation == "addition" or operation == "add":
     message = "Your sum is " + str(add(x,y))
     
 elif operation == "subtraction" or operation == "subtract":
     message = "Your difference is " + str(sub(x,y))
+
+elif operation == "multiplication" or operation == "multiply":
+    message = "Your product is " + str(mul(x,y))
+
+elif operation == "division" or operation == "divide":
+    message = "Your quotient is " + str(div(x,y))
+ 
 else:
     message = "Invalid input"
 
