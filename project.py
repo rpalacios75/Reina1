391ad6dc9707f259350085552e7e894a27cd58c4
EC2 Default User <ec2-user@ip-172-31-30-94.us-east-2.compute.internal>
2019-07-18 17:57:13 +0000

    addition calculator

--- project.py
+++ project.py
@@ -1,1 +1,46 @@
-print ("hello")
+operation = input ("Choose your operation")
+x = input ("What is your first number?")
+# number = int(number)
+y = input ("What is your second number?")
+
+
+def add(x,y):
+    answer = (int(x)+int(y))
+    return (answer)
+
+print ("Your total is " + str(add(x,y)))
+
+
+
+# if operation == "addition":
+#     print ("What is your first")
+    
+
+    
+
+
+
+
+
+# def add(x,y):
+#     answer = (x+y)
+#     val = input("Enter your value: ")
+#     elif:
+#         print ("What do you want to do?")
+#     return "Your total is " + str(answer)
+    
+# print (add(3,2))
+
+#     elif:
+#         print ("What do you want to do?")
+
+
+# print "Your total is" + (add(3,2))
+    #return("The total is " + (x+y))
+
+
+ 
+
+
+
+
